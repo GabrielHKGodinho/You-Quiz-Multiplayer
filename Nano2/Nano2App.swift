85ae916d30176a2b5e4e7592f9332c3754ec7f06
Gabriel Godinho <gabrielgodinho@alunos.utfpr.edu.br>
2023-08-01 08:52:29 -0300

    Initial Commit

---
+++ Nano2/Nano2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano2App.swift
+//  Nano2
+//
+//  Created by Gabriel Henrique Kwiatkovski Godinho on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
